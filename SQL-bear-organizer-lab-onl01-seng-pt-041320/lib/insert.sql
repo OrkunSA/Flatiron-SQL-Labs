f4527f56deddf643275348739231458f1fa33bca
OrkunSA <c.orkun.s@gmail.com>
2020-10-06 19:50:28 -0500

    adding labs

---
+++ SQL-bear-organizer-lab-onl01-seng-pt-041320/lib/insert.sql
@@ -0,0 +1,51 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 5, "Male", "Brown", "Hungry", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Black", "Angry", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "Female", "Red", "Grumpy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "Male", "Black", "Fearless", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "Female", "Brown", "Playful", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 1, "Male", "Brown", "Cuddly", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "Female", "Brown", "Clumsy", 1);
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "Male", "Brown", "Angry", 1);
